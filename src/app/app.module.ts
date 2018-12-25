289c959c333af7d89abfad79a9db510eca410034
alexvlife <alexvlife@gmail.com>
2018-12-25 16:25:53 +0300

    removed unnecessary parts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { WeatherInfoComponent } from './components/weather-info/weather-info.component';
-import { SocialInfoComponent } from './components/social-info/social-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WeatherInfoComponent,
-    SocialInfoComponent
   ],
   imports: [
     BrowserModule
