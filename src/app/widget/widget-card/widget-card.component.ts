cdd669a8c07d5dbb7c8421423d1d6919807051f1
alexvlife <alexvlife@gmail.com>
2018-12-27 18:30:46 +0300

    added logic of init images in WidgetCardComponent

--- src/app/widget/widget-card/widget-card.component.ts
+++ src/app/widget/widget-card/widget-card.component.ts
@@ -10,10 +10,30 @@ export class WidgetCardComponent implements OnInit {
 
   @Input() hotel: Hotel;
 
+  public imagesPath: string = '../../../assets/images/';
+  public defaultImageUrl: string = '../../../assets/images/no-photo.jpg';
+
+  public hotelImageData: {[key: string]: string} = {
+    url1: '',
+    url2: '',
+  };
+
   constructor() { }
 
-  ngOnInit() {
-    console.log('Hotel', this.hotel)
+  ngOnInit(): void {
+    this.initHotelImageData();
   }
 
+  initHotelImageData(): void {
+    if (this.hotel) {
+      this.hotelImageData.url1 = this.hotel.image ?
+        this.getImagePath(this.hotel.image) : this.defaultImageUrl;
+      this.hotelImageData.url2 = (this.hotel.socialInfo && this.hotel.socialInfo.image) ?
+        this.getImagePath(this.hotel.socialInfo.image) : this.defaultImageUrl;
+    }
+  }
+
+  getImagePath(imageName: string): string {
+    return this.imagesPath + imageName;
+  }
 }
